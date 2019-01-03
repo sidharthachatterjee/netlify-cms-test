f403710b9c8d8548a94eebeed25f80968d985e38
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-01-03 19:06:42 +0530

    Add netlify identity

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,8 +62,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: false
+        modulePath: `${__dirname}/src/cms/cms.js`
       }
     },
     'gatsby-plugin-purgecss', // must be after other CSS plugins
